267ceaf8d52e07f04db91069514b643594e91327
antiivka <antiivka@gmail.com>
2025-09-07 16:33:29 +0200

    Create page.jsx

---
+++ app/login/page.jsx
@@ -0,0 +1,10 @@
+// app/login/page.jsx
+export default function LoginPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Login Page</h1>
+      <p>This is a test. If you see this, routing works!</p>
+      <a href="/">Go back home</a>
+    </div>
+  );
+}

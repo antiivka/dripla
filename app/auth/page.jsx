385dd2d1ffbea5bc9e72f2fa748344e1e3ecc51f
antiivka <antiivka@gmail.com>
2025-09-07 16:33:01 +0200

    Delete app/auth directory

--- app/auth/page.jsx
+++
@@ -1,9 +0,0 @@
-// app/auth/page.jsx
-export default function AuthPage() {
-  return (
-    <div>
-      <h1>Auth Page Test</h1>
-      <p>If you can see this, routing works!</p>
-    </div>
-  );
-}
